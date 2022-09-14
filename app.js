de3ab46d1d82f0218025b67450f214f5fc68e6b3
Green Earth Community <39521944+xnarai6@users.noreply.github.com>
2022-09-14 10:09:24 +0900

    khkim: static public 폴더수정

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static(path.resolve('public')));
+app.use('/public', express.static(path.resolve('public')));
 app.get('/robots.txt', (req, res, next) => {
     res.type('text/plain');
     res.send(`
